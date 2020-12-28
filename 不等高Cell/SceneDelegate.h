901621d943b65f5b75ccb455c7a90b094626bbf0
Jinwei <renjwjx@gmail.com>
2020-12-28 08:38:10 +0800

    Initial Commit

---
+++ 不等高Cell/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  不等高Cell
+//
+//  Created by renjinwei on 2020/12/28.
+//  Copyright © 2020 renjinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ 不等高Cell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  不等高Cell
+//
+//  Created by renjinwei on 2020/12/28.
+//  Copyright © 2020 renjinwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
